47ba54a4774c0dba300fa6e8089187df02a2bf45
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2019-09-22 18:36:12 -0300

    [ FEATURE ] Adicionando lógica para buscar questões randomicamente e passagem para a próxima questão.

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -2,6 +2,8 @@ import React, { Fragment, Component } from 'react';
 
 import axios from 'axios';
 
+import { Redirect } from 'react-router-dom';
+
 import { renderWord } from '../../utilities/question';
 
 import { StyledQuiz, Demonstration, Container, ContainerQuestions, Question } from './style/StyledQuiz';
@@ -14,6 +16,9 @@ export default class Quiz extends Component {
     state = {
         answer: '',
         data: '',
+        step: 0,
+        chosenQuestions: [],
+        limiteQuestions: '',
     }
 
     chooseMyAnswer = (answer) => {
@@ -22,19 +27,51 @@ export default class Quiz extends Component {
         })
     };
 
+    renderChosenQuestions = (data) => {
+        const elements = [];
+        const { chosenQuestions } = this.state;
+
+        while(elements.length < data.questoes_randomizar) {
+            data.banco_questoes.map((question, indice) => {
+                let number = Math.floor(Math.random() * data.banco_questoes.length);
+                    
+                if(elements.indexOf(number) === -1) {
+                    elements.push(number)
+                }
+            });
+        }
+
+        return elements;
+    }
+
     async componentDidMount() {
         const { data } = this.state;
 
         const response = await axios.get('/api/quiz.json');
         this.setState({
-            data: response.data,    
+            data: response.data,
+            chosenQuestions: this.renderChosenQuestions(response.data),
+            limiteQuestions: response.data.questoes_randomizar,
         });
     }
+    
+    nextStep = (e) => {
+        e.preventDefault();
+
+        this.setState({
+            step: this.state.step += 1,
+        })
+    };
 
     render() {
-        const { data } = this.state;
+        const { data, chosenQuestions, step, limiteQuestions } = this.state;
 
-        console.log(renderWord(0));
+        console.log(step);
+        console.log(limiteQuestions);
+
+        if(step === limiteQuestions) {
+            return <Redirect to={{ pathname: '/resultado', state: { from: this.props.location } }}/>
+        }
 
         return(
             <StyledQuiz>
@@ -49,27 +86,25 @@ export default class Quiz extends Component {
                         (data) ? (
                             <main>
                                 {
-                                    data.banco_questoes.map(question => (
-                                        <Fragment key={question.id}>
-                                            <section className="question">
-                                                <h3>Pergunta?</h3>
-                                                <p>{question.pergunta}</p>
-                                            </section>  
+                                    <Fragment key={data.banco_questoes[chosenQuestions[step]].id}>
+                                        <section className="question">
+                                            <h3>Pergunta:</h3>
+                                            <p>{data.banco_questoes[chosenQuestions[step]].pergunta}</p>
+                                        </section>  
                                             
-                                            <ContainerQuestions>
-                                            { question.alternativas.map((alternativa, id) => (
+                                        <ContainerQuestions>
+                                            { data.banco_questoes[chosenQuestions[step]].alternativas.map((alternativa, id) => (
                                                 <Question key={alternativa.id}>
                                                     <input type="radio" id={alternativa.id} value={alternativa.impacto_indicadores} name="question" onChange={(e) => this.chooseMyAnswer(e)} />
                                                     <label htmlFor={alternativa.id}>{renderWord(id)}</label>
                                                     <label htmlFor={alternativa.id}>{ alternativa.descricao }</label>
                                                 </Question>    
                                             )) }
-                                            </ContainerQuestions>
-                                            
-                                        </Fragment>
-                                    ))
+                                        </ContainerQuestions>
+                                    </Fragment>
                                 }
-                                <a href="" className="btn btn-advance">Confirmar</a>
+
+                                <a href="" className="btn btn-advance" onClick={(e) => this.nextStep(e)}>Confirmar</a>
                             </main>
                         ) : null
                     }
